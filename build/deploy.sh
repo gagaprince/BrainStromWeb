08d91b853bfa5cd17e25e3cdd8f77919184c6b83
wangzidong <wangzidong@meituan.com>
2017-10-23 16:08:16 +0800

    部署工具

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,26 +2,6 @@
 
 source /etc/profile
 
-echo "begin rebuild"
+sudo tomcat start
 
-echo $PATH
 
-env
-
-cd /root/work/blog/
-
-ls
-
-git pull
-
-mvn clean -Paliyun package
-
-mv target/ROOT.war /usr/share/tomcat7/webapps/
-
-echo 'kill blog'
-
-ps -ef|grep tomcat7 |grep -v grep|awk '{print $2}'|xargs kill -9
-
-cd /usr/share/tomcat7/
-
-sh bin/startup.sh
---
+++ build/preDeploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source /etc/profile
+
+echo "begin rebuild"
+
+echo $PATH
+
+env
+
+cd /home/gagaprince/work/server/BrainStromWeb
+
+ls
+
+git pull
+
+mvn clean -Plocal package
+
+mv target/ROOT.war /usr/share/tomcat/webapps/
+
+echo 'kill BrainStrom'
+
+ps -ef|grep tomcat |grep -v grep|awk '{print $2}'|xargs sudo kill -9
