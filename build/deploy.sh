dfaaa5fa202878747d31f5eaeb091b0b26102767
wangzidong <wangzidong@meituan.com>
2017-10-23 16:14:57 +0800

    更改部署工具

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,6 +2,10 @@
 
 source /etc/profile
 
+echo 'kill BrainStrom'
+
+ps -ef|grep tomcat |grep -v grep|awk '{print $2}'|xargs sudo kill -9
+
 sudo tomcat start
 
 
--- build/preDeploy.sh
+++ build/preDeploy.sh
@@ -16,8 +16,7 @@ git pull
 
 mvn clean -Plocal package
 
+sudo rm -rf /usr/share/tomcat/webapps/ROOT
+
 sudo mv target/ROOT.war /usr/share/tomcat/webapps/
 
-echo 'kill BrainStrom'
-
-ps -ef|grep tomcat |grep -v grep|awk '{print $2}'|xargs sudo kill -9
