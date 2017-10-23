899a090247de8f35d2997e3738dc163d0c6f3b07
wangzidong <wangzidong@meituan.com>
2017-10-23 17:32:51 +0800

    更改部署工具

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,7 +4,7 @@ source /etc/profile
 
 echo 'kill BrainStrom'
 
-ps -ef|grep tomcat |grep -v grep|awk '{print $2}'|xargs sudo kill -9
+killall -9 java
 
 sudo tomcat start
 
--- build/preDeploy.sh
+++ build/preDeploy.sh
@@ -17,7 +17,7 @@ git pull
 mvn clean -Plocal package
 
 #如果是多台机器则要循环执行下列shell
-scp target/ROOT.war gagaprince@wzdmeituan02:/usr/share/tomcat/webapps/
+sudo scp target/ROOT.war root@wzdmeituan02:/usr/share/tomcat/webapps/
 
 ssh gagaprince@wzdmeituan02   << remotessh
 
